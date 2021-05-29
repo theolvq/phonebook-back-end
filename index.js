251ca3c40ae2a1fecadef0ac5935bd93c46089ed
daawascript <70675996+daawascript@users.noreply.github.com>
2021-05-28 19:29:33 -0700

    completed 3.7 & 3.8

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+morgan.token('body', req => JSON.stringify(req.body));
+
 app.use(express.json());
+
+app.use(morgan(':method :url :response-time ms :body'));
 
 let persons = [
   {
@@ -26,25 +31,30 @@ let persons = [
   },
 ];
 
+// Just checking that server is working
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+  res.send('<h1>Welcome to the Phonebook App</h1>');
 });
 
+// get contact list
 app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
+// get specific contact
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find(person => person.id === id);
   person ? res.json(person) : res.status(404).end();
 });
 
+// get info page with number of contacts in phonebook
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p> 
   <p>${new Date()}</p>`);
 });
 
+// delete specific contact
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter(person => person.id !== id);
@@ -52,14 +62,17 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
+// create id
 const generateId = () => {
   const maxId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) : 0;
   return maxId + 1;
 };
 
+// add new contact to the list
 app.post('/api/persons', (req, res) => {
   const body = req.body;
 
+  // error checking
   const alreadyAdded = persons.some(person => person.name === body.name);
 
   if (!body.name) {
@@ -76,6 +89,7 @@ app.post('/api/persons', (req, res) => {
     });
   }
 
+  // create person object to then add in new array
   const person = {
     name: body.name,
     number: body.number,
@@ -87,7 +101,42 @@ app.post('/api/persons', (req, res) => {
   res.json(person);
 });
 
-const PORT = 3002;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// exampple of a request logger middleware
+// const requestLogger = (request, response, next) => {
+//   console.log('Method:', request.method);
+//   console.log('Path:  ', request.path);
+//   console.log('Body:  ', request.body);
+//   console.log('---');
+//   next();
+// };
+
+// app.use(requestLogger);
+// const customLog = morgan(function (tokens, req, res) {
+//   console.log(tokens.method(req, res));
+
+//   if (tokens.method(req, res) === 'GET') {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, 'content-length'),
+//       '-',
+//       tokens['response-time'](req, res),
+//       'ms',
+//     ].join(' ');
+//   }
+//   return [
+//     tokens.method(req, res),
+//     tokens.url(req, res),
+//     tokens.status(req, res),
+//     tokens.res(req, res, 'content-length'),
+//     '-',
+//     tokens['response-time'](req, res),
+//     'ms',
+//   ].join(' ');
+// });
